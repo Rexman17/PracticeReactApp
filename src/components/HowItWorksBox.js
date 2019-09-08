fdb9f5267245060efd1c3f75e3309d9057c4889e
Rexman17 <a.stone1794@gmail.com>
2019-09-08 17:59:54 -0400

    Change HowItWorksBox to class component bc need state

--- src/components/HowItWorksBox.js
+++ src/components/HowItWorksBox.js
@@ -2,30 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import Bullets from './Bullets';
 
-const HowItWorksBox = () => {
-
-  const HowItWorksDiv = styled.div`
-    background: white;
-    border-radius: 5px;
-    margin-top: 3em;
-    padding: 1em;
-  `
-
-  const Title = styled.span`
-    font-weight: bold;
-    font-family: 'Quicksand', sans-serif;
-    letter-spacing: 1px;
-  `
+class HowItWorksBox extends React.Component {
 
 
-  return (
-    <HowItWorksDiv>
-      <Title>
-        HOW IT WORKS
-      </Title>
-      <Bullets />
-    </HowItWorksDiv>
-  )
+  render() {
+
+      const HowItWorksDiv = styled.div`
+        background: white;
+        border-radius: 5px;
+        margin-top: 3em;
+        padding: 1em;
+      `
+
+      const Title = styled.span`
+        font-weight: bold;
+        font-family: 'Quicksand', sans-serif;
+        letter-spacing: 1px;
+      `
+
+      const DownBtn = ({ className, children }) => (
+      <div className={"fa fa-angle-down"} style={{paddingLeft: "10em", fontSize: "36px"}}>
+      </div>
+      )
+
+      const FlexContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+      `
+
+    return (
+      <HowItWorksDiv>
+        <FlexContainer>
+          <Title>
+            HOW IT WORKS
+          </Title>
+          <DownBtn></DownBtn>
+        </FlexContainer>
+        <Bullets />
+      </HowItWorksDiv>
+    )
+  }
+
 }
 
 export default HowItWorksBox;
